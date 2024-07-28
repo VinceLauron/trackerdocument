9861d594cab7237d7db44ee5453844bc51121ffe
Vince Lauron <vince@example.com>
2024-07-28 21:07:59 +0800

    new connection

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,9 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','fms_db')or die("Could not connect to mysql".mysqli_error($con));
+
+$conn = new mysqli('127.0.0.1', 'u510162695_fms_db_root', '1Fms_db_root', 'u510162695_fms_db', 3306);
+
+if ($conn->connect_error) {
+    die("Could not connect to mysql: " . $conn->connect_error);
+}
+?>
