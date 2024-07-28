a1ffe51142f723d9a6e42e03ab2c5ee19551d87e
Vince Lauron <vince@example.com>
2024-07-28 21:24:40 +0800

    ew

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,9 @@
 <?php
-$servername = '127.0.0.1'; // or your database server address
-$username = 'u510162695_fms_db_root';
-$password = '1Fms_db_root';
-$dbname = 'u510162695_fms_db';
-$port = 3306; // or your database port
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli('127.0.0.1', 'u510162695_fms_db_root', '1Fms_db_root', 'u510162695_fms_db', 3307);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Could not connect to mysql: " . $conn->connect_error);
 }
 
 ?>
